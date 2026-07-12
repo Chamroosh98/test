4065ceec31c4f0664fa31abde42ee67e75f80935
chamroosh <monstersec98@gmail.com>
2026-07-12 12:23:32 +0330

    update core/package_resolver

--- core/package_resolver.sh
+++ core/package_resolver.sh
@@ -8,7 +8,6 @@ resolve_packages()
 
     add_final()
     {
-
         pkg="$1"
 
         [ -z "$pkg" ] && return
@@ -20,106 +19,117 @@ resolve_packages()
             ;;
 
         *)
-
             FINAL_PACKAGES="$FINAL_PACKAGES $pkg"
-
             ;;
-
         esac
-
     }
 
+
+
+    #
+    # Base selected packages
+    #
     for pkg in $SELECTED_PACKAGES
     do
-
         add_final "$pkg"
-
     done
 
 
 
-    ###############################################################################
-    # Passwall Dependencies
-    ###############################################################################
-
+    #
+    # Passwall dependencies
+    #
     case "$SELECTED_PROFILE" in
 
 
     passwall|passwall2)
 
 
-    add_final "tcping"
-    add_final "geoview"
+        # dependencies MUST install first
+        add_final "tcping"
+        add_final "geoview"
 
 
-    case "$SELECTED_ENGINE" in
+        case "$SELECTED_ENGINE" in
 
-    xray)
+        xray)
+            add_final "xray-core"
+            ;;
 
-        add_final "xray-core"
 
-    ;;
+        sing-box)
+            add_final "sing-box"
+            ;;
 
-    sing-box)
 
-        add_final "sing-box"
+        auto)
+            add_final "xray-core"
+            ;;
 
-    ;;
+        esac
 
-    auto)
-
-        add_final "xray-core"
 
     ;;
 
     esac
 
 
-    ;;
 
-
-    esac
-
-
-
-    ###############################################################################
+    #
     # Language
-    ###############################################################################
-
+    #
     case "$SELECTED_LANGUAGE" in
 
+
     fa)
-
         add_final "luci-i18n-passwall2-fa"
-
     ;;
+
 
     zh-cn)
-
         add_final "luci-i18n-passwall2-zh-cn"
-
     ;;
+
 
     ru)
-
         add_final "luci-i18n-passwall2-ru"
-
     ;;
+
 
     esac
 
 
 
-    ###############################################################################
-    # Official Geo
-    ###############################################################################
-
+    #
+    # Geo
+    #
     if [ "$SELECTED_GEO" = "official" ]; then
 
         add_final "v2ray-geoip"
         add_final "v2ray-geosite"
 
     fi
+
+
+
+    #
+    # Passwall package LAST
+    #
+    case "$SELECTED_PROFILE" in
+
+    passwall2)
+        add_final "luci-app-passwall2"
+    ;;
+
+
+    passwall)
+        add_final "luci-app-passwall"
+    ;;
+
+
+    esac
+
+
 
     export FINAL_PACKAGES
 
