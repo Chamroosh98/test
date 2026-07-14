a88da5943280c96de07d0dc42951dabd037d8693
chamroosh <monstersec98@gmail.com>
2026-07-14 12:59:02 +0330

    test

--- ui/main_menu.sh
+++ ui/main_menu.sh
@@ -1,48 +1,34 @@
 #!/bin/sh
 
-show_dashboard()
-{
-    echo
-    box_header "🖥  System Information"
-    show_system_info_content
-    box_subheader "🌐 Network"
-    get_network_info_content
-    box_footer
-}
-
 main_menu()
 {
+
     while true
         do
 
             clear
             show_banner
-            show_dashboard
+            show_system_info
+            
 
             echo
             echo "  📦 1) Install Package"
-            echo "  🌐 2) Network Diagnostics"
-            echo "  🚪 3) Exit"
+            echo "  🚪 2) Exit"
             echo
+
 
             printf "  ⁉️ Choice : "
             read -r choice </dev/tty
+
 
             case "$choice" in
 
                 1)
                     package_menu || exit 1
                     exit 0
-                    ;;
+                ;;
 
                 2)
-                    network_check
-                    echo
-                    printf "  ⏎  برای بازگشت Enter بزن..."
-                    read -r _ </dev/tty
-                    ;;
-
-                3)
                     exit 0
                     ;;
 
@@ -55,5 +41,3 @@ main_menu()
 
         done
 }
-
-main_menu
