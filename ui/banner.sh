41d0742a714da7e5030159e6e45a28f483f02d85
chamroosh <monstersec98@gmail.com>
2026-07-23 15:53:37 +0330

    [up] banner

--- ui/banner.sh
+++ ui/banner.sh
@@ -2,6 +2,22 @@
 
 show_banner()
 {
+    detect_arch
+    
+    OW_VER="Unknown"
+    if [ -f /etc/openwrt_release ]; then
+        . /etc/openwrt_release
+        OW_VER="${DISTRIB_RELEASE:-Unknown}"
+    fi
+
+    TOTAL_RAM_MB=$(get_total_ram_mb)
+    FREE_RAM_MB=$(get_free_ram_mb)
+    USED_RAM_MB=$((TOTAL_RAM_MB - FREE_RAM_MB))
+
+    TOTAL_STO_MB=$(get_total_storage_mb)
+    FREE_STO_MB=$(get_free_storage_mb)
+    USED_STO_MB=$((TOTAL_STO_MB - FREE_STO_MB))
+
     echo
 
     L1="    ____              ____"
@@ -10,24 +26,13 @@ show_banner()
     L4="   |____/ \__,_|\__, |_|   \__,_|___/___/"
     L5="                |___/"
 
-    printf " ${CYAN}%-43s${RESET}  ${GRAY}│${RESET}  ${BOLD}DayPass Package Deployer${RESET} (${YELLOW}%s${RESET})\n" \
-        "$L1" "${VERSION:-v2.1.0}"
+    printf " ${CYAN}%-42s${RESET}  ${GRAY}🐱 github.com/Chamroosh98${RESET}\n" "$L1"
+    printf " ${CYAN}%-42s${RESET}  ${CYAN}🩻 Architecture :${RESET} %s\n" "$L2" "$ARCH"
+    printf " ${CYAN}%-42s${RESET}  ${CYAN}💡 OpenWrt      :${RESET} %s\n" "$L3" "$OW_VER"
+    printf " ${CYAN}%-42s${RESET}  ${CYAN}🧠 Memory       :${RESET} %s/%s MB\n" "$L4" "$USED_RAM_MB" "$TOTAL_RAM_MB"
+    printf " ${CYAN}%-42s${RESET}${YELLOW}%-8s${RESET} ${CYAN}💾 Storage      :${RESET} %s/%s MB\n" "$L5" "${VERSION:-v2.1.0}" "$USED_STO_MB" "$TOTAL_STO_MB"
 
-    printf " ${CYAN}%-43s${RESET}  ${GRAY}│${RESET}  ${GRAY}An OpenWrt Deployment Engine${RESET}\n" \
-        "$L2"
-
-    printf " ${CYAN}%-43s${RESET}  ${GRAY}│${RESET}  ${GRAY}🐱 github.com/Chamroosh98${RESET}\n" \
-        "$L3"
-
-    printf " ${CYAN}%-43s${RESET}  ${GRAY}│${RESET}\n" \
-        "$L4"
-
-    printf " ${CYAN}%-43s${RESET}  ${GRAY}│${RESET}\n" \
-        "$L5"
-
-    printf "${GRAY} ────────────────────────────────── 🕊️  Remembering of the IRAN Massacre on Jan 8-9, 2026 ──────────────────────────────────────── ${RESET}\n"
-    # printf "   ${RED}🕊️  Remembering of the IRAN massacre (Jan 8-9, 2026)${RESET}\n"
-    # printf "${GRAY} ────────────────────────────────────────────────────────────────────────── ${RESET}\n"
-    
+    echo
+    printf "${GRAY}─────────────── 🕊️  Remembering the IRAN Massacre on Jan 8-9, 2026 ───────────────${RESET}\n"
     echo
 }
